a33054edb8903fdc6b35b904aac945498dbba166
Philipp Giese <philippgiese@Philipps-Air.fritz.box>
2018-12-18 15:56:44 +0100

    Small correction regarding the calculation of the BTC-price

--- R/thermocap.R
+++ R/thermocap.R
@@ -15,6 +15,6 @@ function(){
   emission<-jsonlite::fromJSON("https://api.blockchain.info/charts/total-bitcoins?timespan=all&format=json")$values
   emission<-xts::xts(emission$y, order.by = as.Date(as.POSIXct(emission$x, origin="1970-01-01")))
   emission<-diff(emission)
-  price<-btcecho::calculate_price("CCCAGG","BTC","USD",2000)$close
+  price<-btcecho::calculate_switch_cc("CCCAGG","BTC","USD",2000,F)$close
   cumsum(emission*price)/10^9
 }
